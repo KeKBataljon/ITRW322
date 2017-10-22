fe38dad5927f59ea8d68067ae97e4e621e71d802
Christo du Plessis <duplessis.christo3616@gmail.com>
2017-10-22 13:41:21 +0200

    Add files via upload

--- EchoDesktopApp/EchoDesktopApp/frmSettings.Designer.cs
+++ EchoDesktopApp/EchoDesktopApp/frmSettings.Designer.cs
@@ -28,33 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.tbURL = new System.Windows.Forms.TextBox();
+            this.tbPort = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tbURL
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(31, 68);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(227, 29);
-            this.textBox1.TabIndex = 0;
+            this.tbURL.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbURL.Location = new System.Drawing.Point(31, 61);
+            this.tbURL.Name = "tbURL";
+            this.tbURL.Size = new System.Drawing.Size(227, 29);
+            this.tbURL.TabIndex = 0;
             // 
-            // textBox2
+            // tbPort
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(31, 162);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(227, 29);
-            this.textBox2.TabIndex = 1;
+            this.tbPort.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbPort.Location = new System.Drawing.Point(31, 149);
+            this.tbPort.Name = "tbPort";
+            this.tbPort.Size = new System.Drawing.Size(227, 29);
+            this.tbPort.TabIndex = 1;
+            this.tbPort.Text = "8080";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(27, 31);
+            this.label1.Location = new System.Drawing.Point(27, 24);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(99, 24);
             this.label1.TabIndex = 2;
@@ -64,11 +66,21 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(27, 126);
+            this.label2.Location = new System.Drawing.Point(27, 113);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(43, 24);
             this.label2.TabIndex = 3;
             this.label2.Text = "Port";
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(182, 196);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 4;
+            this.btnSubmit.Text = "&Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // frmSettings
             // 
@@ -76,10 +88,11 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.ClientSize = new System.Drawing.Size(284, 231);
+            this.Controls.Add(this.btnSubmit);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbPort);
+            this.Controls.Add(this.tbURL);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.Name = "frmSettings";
             this.Text = "Settings";
@@ -91,9 +104,10 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox tbURL;
+        private System.Windows.Forms.TextBox tbPort;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
--- EchoDesktopApp/EchoDesktopApp/frmSettings.cs
+++ EchoDesktopApp/EchoDesktopApp/frmSettings.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;//proxy settings
 
 namespace EchoDesktopApp
 {
     public partial class frmSettings : Form
     {
+        WebClient client = new WebClient();
         public frmSettings()
         {
             InitializeComponent();
@@ -20,6 +22,40 @@ namespace EchoDesktopApp
         private void frmSettings_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            //try to check if number entered
+            try
+            {
+                int numTest = Convert.ToInt32(tbPort);
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(null, "Please ensure the port is of number type", "Port number error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                setProxy(tbURL.Text, tbPort.Text);
+            }
+           
+         }
+
+        public void setProxy(String url, String port)
+        {
+            //ensure system.net is added as a resource to project
+            if (Uri.IsWellFormedUriString(tbURL.Text, UriKind.RelativeOrAbsolute))//ensures address is valid
+            {
+                WebProxy proxy = new WebProxy();
+                proxy.Address = new Uri(url + ":" + port);//port not required so specifying default as 8080 in textbox
+                proxy.Credentials = new NetworkCredential("username", "password");
+                proxy.UseDefaultCredentials = false; //true for no username and password
+                proxy.BypassProxyOnLocal = false;  //still use the proxy for local addresses
+
+                client.Proxy = proxy;
+                //String test = frmSetting.client.Proxy.ToString(); use this to get proxy in other classes
+            }
         }
     }
 }
